0ec936a9e4f174271eca9ecd12b12e2c7e831451
Reya Vir <reyavir@gmail.com>
2024-06-22 12:35:18 -0700

    Initial Commit

---
+++ app/Managers/LocationManager.swift
@@ -0,0 +1,13 @@
+//
+//  LocationManager.swift
+//  app
+//
+//  Created by Reya Vir on 6/22/24.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+}
---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Reya Vir on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
