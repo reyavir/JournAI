2df26337b73623de6a6f438eda441fcf256a3cb7
Reya Vir <reyavir@gmail.com>
2024-06-22 15:35:16 -0700

    journal entry and project data

---
+++ app/Views/JournalEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  JournalEntryView.swift
+//  app
+//
+//  Created by Reya Vir on 6/22/24.
+//
+
+import SwiftUI
+
+struct JournalEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JournalEntryView()
+}
