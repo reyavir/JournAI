2f7c589846a794c39cd6ceb420e46f00dfcf0462
Reya Vir <reyavir@gmail.com>
2024-06-22 21:53:16 -0700

    added suggestions to entry page

--- app/Views/JournalEntryView.swift
+++ app/Views/JournalEntryView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct JournalEntryView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+    @State var journalEntry: String = "Type your journal entry here"
+    @State var currentDate = Date()
+    @State var title = "Journal Entry"
+        
+        var body: some View {
+            
+            VStack {
+               
+                TextField("Journal Entry", text: $title)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                            
+                Text(formattedDate)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                
+                TextEditor(text: $journalEntry)
+                    .frame(minHeight: 200)
+                    .padding()
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Journal Entry")
+        }
+    
+        var formattedDate: String {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .long
+            formatter.timeStyle = .none
+            return formatter.string(from: currentDate)
+        }
 }
 
 #Preview {
