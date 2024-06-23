fd911402769034c78b8cfb848fd18a9c03974eec
Reya Vir <reyavir@gmail.com>
2024-06-22 17:57:47 -0700

    added animation

---
+++ app/Views/LocationView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationView.swift
+//  app
+//
+//  Created by Reya Vir on 6/22/24.
+//
+
+import SwiftUI
+
+struct LocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationView()
+}
---
+++ app/Views/SiriAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  SiriAnimation.swift
+//  app
+//
+//  Created by Reya Vir on 6/22/24.
+//
+
+import SwiftUI
+
+struct SiriAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SiriAnimation()
+}
