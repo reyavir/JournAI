808d8517beb8b415bf600c379fd53a3ea57855ce
Reya Vir <reyavir@gmail.com>
2024-06-22 17:59:18 -0700

    added welcome page and animation

--- app/Views/WelcomeView.swift
+++ app/Views/WelcomeView.swift
@@ -6,30 +6,71 @@
 //
 
 import SwiftUI
-import CoreLocationUI
 
 struct WelcomeView: View {
-    @EnvironmentObject var locationManager: LocationManager
+    @State var username = ""
+    @State var password = ""
     
     var body: some View {
-        VStack {
-            VStack(spacing: 20) {
-                Text("Welcome").bold().font(.title)
-                Text("Please share your current location").padding()
+        NavigationView {
+            Form {
+                SectionList(entries: [
+                    ("Journal Entries", "book.closed", Color.blue),
+                    ("Health Tracking", "heart.circle", Color.red),
+                    ("Weekly Goals", "flag.circle", Color.green),
+                    ("Mood Tracking", "face.smiling", Color.yellow),
+                    ("Exercise Log", "figure.walk", Color.orange),
+                    ("Meal Planner", "fork.knife", Color.purple),
+                    ("Sleep Analysis", "bed.double", Color.indigo)
+                ])
             }
-            .multilineTextAlignment(.center)
-            .padding()
+//            .navigationTitle("Dashboard")
             
-            LocationButton(.shareCurrentLocation) {
-                locationManager.requestLocation()
+            VStack {
+                Text("Welcome to [App Name]")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Spacer()
+
+                VStack(spacing: 20) {
+                    TextField("Username", text: $username)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+                    
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal)
+                }
+                .padding()
+
+                Spacer()
             }
-            .cornerRadius(30)
-            .symbolVariant(.fill)
-            .foregroundColor(.white)
-        }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+            .padding()
+        }
+        
+    }
+}
+
+struct SectionList: View {
+    let entries: [(String, String, Color)]
+    
+    var body: some View {
+        ForEach(entries, id: \.0) { entry in
+            Section {
+                Label {
+                    Text(entry.0)
+                        .foregroundColor(.black)
+                } icon: {
+                    Image(systemName: entry.1)
+                        .foregroundColor(entry.2)
+                }
+            }
+        }
     }
 }
 
 #Preview {
     WelcomeView()
 }
+
